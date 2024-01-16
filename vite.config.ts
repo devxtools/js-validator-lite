1546a8056bfad678f15dde01f71340a65a717a95
凡尘 <335296558@qq.com>
2024-01-17 01:11:23 +0700

    add README.md

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
+        minify: 'esbuild',
         lib: {
             entry: {
                 index: path.resolve(__dirname, 'src/index.ts'),
